343fbf49d7603308633d233b2874e97e47e7b876
shemigam1 <97962478+shemigam1@users.noreply.github.com>
2022-10-20 14:07:09 +0000

    added game logic and user interface

--- index.js
+++ index.js
@@ -1,19 +1,70 @@
-let user
- user = prompt('Hi, what is your username? ')
-console.log(`Hi ${user}`)
+let username;
+username = prompt("Hi, what is your username? ");
+console.log(`Hi ${username}`);
 
-let min = 1
-let max
-max = 5
-let sayRange = `${min} to ${max}`
-let computerGuess = 3
-let userGuess
-userGuess = prompt(`your range is ${sayRange} choose a number: `)
+let range = [1, 2]
+let lastRange = range[range.length -1] 
+let points = 0
+let stage = 1
+console.log(`stage ${stage}, range is 1 to ${lastRange}, you've got ${points} points`)
 
-let points = 0 
-if (userGuess == computerGuess) {
-  points++
-  console.log(`you got ${points} points`)
-} else {
-  console.log(`Game over you got ${points} points`)
+// console.log(lastRange)
+
+
+const game = function() {
+  let userGuess = prompt('Guess a number: ')
+  if(userGuess<1 || userGuess > lastRange){
+    console.log('Enter a value within the range')
+    game()
+  }
+
+  let computerGuess = Math.floor(Math.random() * range.length + 1);
+  const ifCorrect = (replay) => {
+    if (replay == true) {
+      console.log(`stage ${stage}, range is 1 to ${lastRange}`)
+    }else {
+    lastRange++
+    range.push(lastRange)
+    console.log(range)
+    points++
+    stage++
+    console.log(`stage ${stage}, range is 1 to ${lastRange}`)
+    console.log(`Congratulations🏆🙌, you've got ${points} points`)
+    }
+    game()
+    
+  }
+  
+  const ifNotCorrect = () => {
+
+      console.log(`sorry😭, game over, you got've ${points} points`)
+  }
+  
+
+ const gameLogic = () => {
+    if (userGuess == computerGuess){
+  ifCorrect(false)
+    
+  } else {
+  ifNotCorrect()
+      let tryAgain = prompt('continue anyways? say yes or no ')
+    if (tryAgain.toLowerCase() == 'yes') {
+      ifCorrect(true)
+      
+    }else if (tryAgain.toLowerCase() == 'no') {
+      console.log('GG. Good Game... try again later👍 press ctrl + c to exit ')
+      
+    }else {
+      console.log('type valid input')
+    }
+    
+  }
+ }
+  for(let i = 1; i<range.length; i++ ) {
+    gameLogic()
+    
+    break
+    
+  }
 }
+game()
